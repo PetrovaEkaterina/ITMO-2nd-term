54ab6231d67dca70dbc9a92570445b1113eff6e7
Sc4ramouche <vladislav.kovechenkov@gmail.com>
2018-05-05 15:35:07 +0300

    initial

---
+++ 30-03-2018/async-task-1.js
@@ -0,0 +1,28 @@
+const values = [10, 20, 30];
+const URLs = [
+  'https://kodaktor.ru/api/m/',
+  'https://kodaktor.ru/api/MS2/',
+  'https://kodaktor.ru/api/MS3/',
+];
+
+const r = {
+  result: "10"
+};
+
+(async () => { 
+  let init = {
+    method: 'GET',
+    headers: {
+      'content-type': 'application/json'
+    }
+  };
+  
+  for (let i = 0; i < URLs.length; i++) {
+    let value = URLs[i] + values[i] + '/' + r.result;
+    const x = await fetch(value, init)
+      .then(response => response.json());
+      r.result = x.result;
+  }
+  
+  document.body.innerHTML = `<h1>${r.result}</h1>`;
+})();
